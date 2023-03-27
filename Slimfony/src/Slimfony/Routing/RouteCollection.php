1ca338d0b47d8a3296915df9b7f40c6bb9818846
slavjuan <koenbeerta@gmail.com>
2023-03-27 17:34:07 +0200

    RouteCollection

--- Slimfony/src/Slimfony/Routing/RouteCollection.php
+++ Slimfony/src/Slimfony/Routing/RouteCollection.php
@@ -6,4 +6,45 @@ use Slimfony\HttpFoundation\Request;
 
 class RouteCollection
 {
+    /**
+     * @var array<Route>
+     */
+    protected array $routes;
+
+    /**
+     * @param array<Route> $routes
+     */
+    public function __construct(array $routes=[])
+    {
+        $this->routes = $routes;
+    }
+
+    /**
+     * @param Route $route
+     * @return void
+     */
+    public function add(Route $route): void
+    {
+        $this->routes[] = $route;
+    }
+
+    /**
+     * @param Route $route
+     * @return void
+     */
+    public function remove(Route $route): void
+    {
+        if (false !== $key = array_search($route, $this->routes)) {
+            unset($this->routes[$key]);
+        }
+    }
+
+    /**
+     * Returns all routes
+     * @return Array<Route>
+     */
+    public function all(): array
+    {
+        return $this->routes;
+    }
 }
--- slimfonytest/public/index.php
+++ slimfonytest/public/index.php
@@ -5,10 +5,23 @@ require dirname(__DIR__).'/vendor/autoload.php';
 // For example:
 use Slimfony\Routing\Route;
 use Slimfony\Config\ConfigLoader;
+use Slimfony\Routing\RouteCollection;
 
 $config = new ConfigLoader(dirname(__DIR__));
 
+$route1 = new Route('test', 'test', 'test ', ['test']);
+$route2 = new Route('foo', 'test', 'test ', ['test']);
+
+$routeCollection = new RouteCollection();
+
+$routeCollection->add($route1);
+$routeCollection->add($route2);
+
+$routeCollection->remove($route2);
+
+var_dump($routeCollection);
+
 //var_dump(new Route('test', 'test', 'test ', ['test']));
-var_dump($config->getRoutes());
+//var_dump($config->getRoutes());
 
 //(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
