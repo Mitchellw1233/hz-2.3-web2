cb4b25c6352c7e4f1d55088362e7536ca390e28d
Mitchellw1233 <mitchellwolters1233@gmail.com>
2023-03-27 13:45:09 +0200

    fix docker modules & fix configloader

--- Slimfony/src/Slimfony/Config/ConfigLoader.php
+++ Slimfony/src/Slimfony/Config/ConfigLoader.php
@@ -5,43 +5,64 @@ namespace Slimfony\Config;
 class ConfigLoader
 {
     /**
-     * @var array{
-     *      route: array {
-     *          path: string,
-     *          controller: string,
-     *          methods: array<string>,
-     *      }
-     * }
+     * @var array<int, array{
+     *     path: string,
+     *     controller: string,
+     *     methods: array<string>|null,
+     * }>
      */
     protected array $routes;
 
     /**
-     * @throws \Exception
+     * @throws \LogicException
      */
-    public function __construct()
+    public function __construct(string $projectDir)
     {
-        if (!file_exists(__DIR__."./config/config.json")) {
-            // TODO: correcte Exception ofzo?
-            throw new \Exception("Could not find config file in root dir");
+        // TODO: Dependency injection of kernel with getProjectDir() or argument injection
+        if (!file_exists($projectDir.'/config/config.json')) {
+            throw new \LogicException('Could not find config file in root dir');
         }
 
-        $file = readfile(__DIR__."./config/config.json");
-        $json = json_decode($file);
+        $file = file_get_contents($projectDir.'/config/config.json');
+        try {
+            $json = json_decode($file, true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException) {
+            throw new \LogicException('Could not correctly parse config.json from json');
+        }
 
-        $this->routes = $json['routes'];
+        $this->setRoutes($json['routes']);
     }
 
     /**
-     * @return array{
-     *      route: array {
-     *          path: string,
-     *          controller: string,
-     *          methods: array<string>,
-     *      }
-     * }
+     * @return array<int, array{
+     *     path: string,
+     *     controller: string,
+     *     methods: array<string>|null,
+     * }>
      */
     public function getRoutes(): array
     {
         return $this->routes;
+    }
+
+    /**
+     * @param array<int, array{
+     *     path: string,
+     *     controller: string,
+     *     methods: array<string>|null,
+     * }> $routes
+     */
+    protected function setRoutes(array $routes): void
+    {
+        $data = [];
+        foreach ($routes as $route) {
+            // Check if methods is set, else empty array
+            if (!isset($route['methods'])) {
+                $route['methods'] = null;
+            }
+            $data[] = $route;
+        }
+
+        $this->routes = $data;
     }
 }
--- slimfonytest/public/index.php
+++ slimfonytest/public/index.php
@@ -6,11 +6,9 @@ require dirname(__DIR__).'/vendor/autoload.php';
 use Slimfony\Routing\Route;
 use Slimfony\Config\ConfigLoader;
 
-$config = new ConfigLoader();
+$config = new ConfigLoader(dirname(__DIR__));
 
-var_dump(new Route('test', 'test', 'test ', ['test']));
+//var_dump(new Route('test', 'test', 'test ', ['test']));
 var_dump($config->getRoutes());
-
-//require dirname(__DIR__).'/vendor/autoload.php';
 
 //(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
