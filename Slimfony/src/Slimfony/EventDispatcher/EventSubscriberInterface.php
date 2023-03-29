9ecc791c5db62559e418eb335188573456ffb682
Mitchellw1233 <mitchellwolters1233@gmail.com>
2023-03-29 19:17:09 +0200

    added eventsubscriber interface

---
+++ Slimfony/src/Slimfony/EventDispatcher/EventSubscriberInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Slimfony\EventDispatcher;
+
+interface EventSubscriberInterface
+{
+    /**
+     * Supported formats:
+     * * ['eventName' => 'methodName']
+     * * ['eventName' => ['methodName', $priority]]
+     *
+     * @return array<string, string|array{0: string, 1?: int}>
+     */
+    public static function getSubscribedEvents(): array;
+}
