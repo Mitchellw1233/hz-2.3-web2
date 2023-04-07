54d7290859035a505246c8311c6b03e1c274ad5c
slavjuan <koenbeerta@gmail.com>
2023-04-07 12:24:25 +0200

    Add more column options

--- Slimfony/src/Slimfony/ORM/Mapping/Column.php
+++ Slimfony/src/Slimfony/ORM/Mapping/Column.php
@@ -8,7 +8,9 @@ class Column
     public function __construct(
         public string $name,
         public string $type,
+        public bool $primaryKey=false,
         public bool $nullable=false,
+        public bool $unique=false,
         public bool $unsigned=false,
         public bool $autoIncrement=false,
     ) {
