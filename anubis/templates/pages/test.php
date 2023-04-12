e5b64f512dd3235de31471be6a5dda900db4ba03
Mitchellw1233 <mitchellwolters1233@gmail.com>
2023-04-12 22:37:01 +0200

    remove unused code

--- anubis/templates/pages/test.php
+++
@@ -1,9 +0,0 @@
-<?php include dirname(__DIR__).'/util/common/ide_helper.php'; ?>
-
-<?php $start_block('body'); ?>
-
-<h1><?php echo $global['basePath'] ?></h1>
-
-<?php $end_block() ?>
-
-<?php require $global['basePath'].'util/common/main.php'; ?>
